da4e2cf4bac192235b0b7cc7a3c7fb2229a7c1d8
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2020-07-05 00:45:41 +0900

    Update M5_MIDI_Receiver.ino

--- M5_MIDI_Receiver/M5_MIDI_Receiver.ino
+++ M5_MIDI_Receiver/M5_MIDI_Receiver.ino
@@ -1,3 +1,9 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
 #include <M5Stack.h>
 #include "ScrollTextWindow.h"
 
